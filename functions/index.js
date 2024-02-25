7f114749cd8c12693e0784b20039958e3d543e4f
Sergiy Misyuk <51460801+smisyuk4@users.noreply.github.com>
2024-02-26 01:50:59 +0200

    change eslint, add some test routes

--- functions/index.js
+++ functions/index.js
@@ -1,62 +1,45 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-// const {onRequest} = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-// The Cloud Functions for Firebase SDK to create Cloud Functions and triggers.
-const {logger} = require("firebase-functions");
 const {onRequest} = require("firebase-functions/v2/https");
-const {onDocumentCreated} = require("firebase-functions/v2/firestore");
+const express = require("express");
+// const {logger} = require("firebase-functions");
+// const {onDocumentCreated} = require("firebase-functions/v2/firestore");
 
 // The Firebase Admin SDK to access Firestore.
 const {initializeApp} = require("firebase-admin/app");
 const {getFirestore} = require("firebase-admin/firestore");
 
 initializeApp();
+const app = express();
 
-// Take the text parameter passed to this HTTP endpoint and insert it into
-// Firestore under the path /messages/:documentId/original
-exports.addmessage = onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
+// build multiple CRUD interfaces:
+app.get("/hello-world", (req, res) =>
+  res.status(200).json({message: "i am from api"})
+);
+
+app.post("/api/messages", async (req, res) => {
   const writeResult = await getFirestore()
-      .collection("messages")
-      .add({original: original});
+    .collection("messages")
+    .add({text: req.body.text});
   // Send back a message that we've successfully written the message
   res.json({result: `Message with ID: ${writeResult.id} added.`});
 });
+// app.put("/:id", (req, res) =>
+//   res.send(Widgets.update(req.params.id, req.body))
+// );
+// app.delete("/:id", (req, res) => res.send(Widgets.delete(req.params.id)));
+// app.get("/", (req, res) => res.send(Widgets.list()));
 
-// Listens for new messages added to /messages/:documentId/original
-// and saves an uppercased version of the message
-// to /messages/:documentId/uppercase
-exports.makeuppercase = onDocumentCreated("/messages/{documentId}", (event) => {
-  // Grab the current value of what was written to Firestore.
-  const original = event.data.data().original;
+// Expose Express API as a single Cloud Function:
+exports.app = onRequest(app);
 
-  // Access the parameter `{documentId}` with `event.params`
-  logger.log("Uppercasing", event.params.documentId, original);
-
-  const uppercase = original.toUpperCase();
-
-  // You must return a Promise when performing
-  // asynchronous tasks inside a function
-  // such as writing to Firestore.
-  // Setting an 'uppercase' field in Firestore document returns a Promise.
-  return event.data.ref.set({uppercase}, {merge: true});
-});
+// // Take the text parameter passed to this HTTP endpoint and insert it into
+// // Firestore under the path /messages/:documentId/original
+// exports.addmessage = onRequest(async (req, res) => {
+//   // Grab the text parameter.
+//   const original = req.query.text;
+//   // Push the new message into Firestore using the Firebase Admin SDK.
+//   const writeResult = await getFirestore()
+//     .collection("messages")
+//     .add({original: original});
+//   // Send back a message that we've successfully written the message
+//   res.json({result: `Message with ID: ${writeResult.id} added.`});
+// });
